8a35b646b995e0dc450729f176390d981c7a4551
ChristianHeck <christianpaulheck@hotmail.com>
2022-08-30 20:01:15 -0300

    Add project files.

---
+++ RESERVA-DE-ESPECTACULO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RESERVA_DE_ESPECTACULO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
